d1e193c69e42a91360618d1aedf9359bae72689e
Stefan Kendall <stefankendall@gmail.com>
2013-10-06 20:44:54 -0400

    Initial app checkin

---
+++ CatSphero/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CatSphero
+//
+//  Created by Stefan Kendall on 10/6/13.
+//  Copyright (c) 2013 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CatSphero/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  CatSphero
+//
+//  Created by Stefan Kendall on 10/6/13.
+//  Copyright (c) 2013 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
